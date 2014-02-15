7d991165585378ef6904b2af3b100f016af6fa8e
Christopher Hiller <chiller@decipherinc.com>
2014-02-15 00:12:59 -0800

    killing the banner. maybe I'll put it back later

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,11 +6,6 @@ module.exports = function (grunt) {
   grunt.initConfig({
     // Metadata.
     pkg: grunt.file.readJSON('package.json'),
-    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
-      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
-      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-      ' Licensed <%= pkg.license %> */\n',
     // Task configuration.
     jshint: {
       options: {
@@ -67,9 +62,6 @@ module.exports = function (grunt) {
       }
     },
     uglify: {
-      options: {
-        banner: '<%= banner %>'
-      },
       main: {
         files: {
           'jquery.preempt.min.js': ['<%= pkg.main %>']
